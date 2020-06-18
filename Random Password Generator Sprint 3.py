a1f5f4b8f652648756e423293263bd5fcb8502a2
Thamsanqa1292 <60877787+Thamsanqa1292@users.noreply.github.com>
2020-06-18 16:07:10 +0200

    Add files via upload

---
+++ Random Password Generator Sprint 3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 14:22:01 2020
+
+@author: Thamsanqa
+"""
+# randon-pasword-
+#Random Password generator using python
+import random
+
+alphabets = ["A","B","C","D","E","F"]
+
+smallAlpha = ['a','b','c','d','e','f']
+
+numbers =[1,2,3,4,5,6,7,8,9,0]
+
+chars = ['!','@','#','$']
+
+ran_1 = random.choice(alphabets) + random.choice(smallAlpha) + str(random.choice(numbers)) + random.choice(chars)
+ran_2 = random.choice(alphabets) + random.choice(smallAlpha) + str(random.choice(numbers)) + random.choice(chars)
+print('Your Random Password is:  ',ran_1 + ran_2)
